4f2d4659260c93b72c77c8a880a8c4776027bd9d
Serggiyenko Mykyta <sergyenko1995@gmail.com>
2022-08-15 21:15:32 +0300

    hjgf

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,15 +2,20 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const idVideo = document.querySelector('#vimeo-player');
-
+const obj = {
+  name: 'gfdsgfds',
+};
+obj.name;
+const { name } = obj;
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const KEYVIDEO = 'videoplayer-current-time';
+
 player.on(
   'timeupdate',
-  throttle(event => {
-    localStorage.setItem(KEYVIDEO, event.seconds);
+  throttle(({ seconds }) => {
+    localStorage.setItem(KEYVIDEO, seconds);
   }, 1000)
 );
-
-player.setCurrentTime(localStorage.getItem(KEYVIDEO));
+const checkLS = localStorage.getItem(KEYVIDEO);
+checkLS && player.setCurrentTime(checkLS);
